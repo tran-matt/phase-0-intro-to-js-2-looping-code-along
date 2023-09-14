71c9f33ca5e05b7d2b5feac2da1b48fafe3aa7e6
Matthew Tran <matt.h.tran@gmail.com>
2023-09-14 17:36:50 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names){
+    const thankYouMessages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`; 
+      thankYouMessages.push(message);
+    }
+    
+    return thankYouMessages;
+  }
+  
+  function countDown(number){
+    for(let count = number ; count >= 0 ; count --){
+        console.log(count);  
+    }
+  }
